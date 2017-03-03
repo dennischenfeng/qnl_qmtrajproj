dbecccdfe4fddf3f6ff0130797c5c167717bcc94
Dennis Feng <dennisfeng@berkeley.edu>
2017-03-02 17:46:42 -0800

    finished NaiveInt's fit method

--- traj_classification.py
+++ traj_classification.py
@@ -41,7 +41,6 @@ class NaiveInt:
         numTraj = [0, 0, 0, 0]  #num trajectories for each label; indices: labelIndex (aka label)
         traj_separated = [[], [], [], []] # trajectories separated into 4 groups based on label; indices: [label][trajIndex][iqIndex, timeIndex]
 
-
         for trajIndex in np.arange(numTotalTraj):
             label = int(labels[trajIndex])
             numTraj[label] = numTraj[label] + 1
@@ -69,6 +68,7 @@ class NaiveInt:
         self.hist2d_exc, _ , _ = np.histogram2d(intTraj_exc_I, intTraj_exc_Q, bins=numBins, range=[[minIntTraj_I, maxIntTraj_I], [minIntTraj_Q, maxIntTraj_Q]])
 
 
+
         #: plot the (non-rotated) plots
         if suppressPlots == False:
             coordsX, coordsY = np.meshgrid(self.binEdges_I, self.binEdges_Q)
@@ -85,12 +85,10 @@ class NaiveInt:
             axarr[0, 2].plot(self.binEdges_Q[:-1], self.hist2d_exc.sum(0))
             axarr[0, 2].set_title('Integrated Q')
 
-            fig.subplots_adjust(hspace=0)
-            plt.show()
+            # plt.show()
 
 
         #: if doRotate is true, then calculate the rotated histograms
-
         if doRotate:
             self.theta = np.arctan2(intTraj_exc_I.mean() - self.intTraj[0][0, :].mean(), intTraj_exc_Q.mean() - self.intTraj[0][1, :].mean())
             self.intTraj_rotated = [np.zeros([2, numTraj[0]]), np.zeros([2, numTraj[1]]), np.zeros([2, numTraj[2]]), np.zeros([2, numTraj[3]])] #indices: [label][iqIndex, trajIndex]
@@ -98,16 +96,62 @@ class NaiveInt:
             for label in [0, 1, 2, 3]:
                 mag = np.sqrt(self.intTraj[label][0,:]**2 + self.intTraj[label][1,:]**2) #the the magnitude (length) of each IQ vector in I-Q space
                 phi = np.arctan2(self.intTraj[label][0,:], self.intTraj[label][1,:])
-                self.intTraj_rotated[label][0,:] = mag * np.cos(phi - theta)
-                self.intTraj_rotated[label][1,:] = mag * np.sin(phi - theta)
+                self.intTraj_rotated[label][0,:] = mag * np.cos(phi - self.theta)
+                self.intTraj_rotated[label][1,:] = mag * np.sin(phi - self.theta)
 
-        # @@@ plot 2d hists
+            #: calculate 2d hists (rotated)
+            minIntTraj_I_rotated = np.min([np.min(self.intTraj_rotated[0][0, :]), np.min(self.intTraj_rotated[1][0, :]), np.min(self.intTraj_rotated[2][0, :]), np.min(self.intTraj_rotated[3][0, :])])
+            maxIntTraj_I_rotated = np.max([np.max(self.intTraj_rotated[0][0, :]), np.max(self.intTraj_rotated[1][0, :]), np.max(self.intTraj_rotated[2][0, :]), np.max(self.intTraj_rotated[3][0, :])])
+            minIntTraj_Q_rotated = np.min([np.min(self.intTraj_rotated[0][1, :]), np.min(self.intTraj_rotated[1][1, :]), np.min(self.intTraj_rotated[2][1, :]), np.min(self.intTraj_rotated[3][1, :])])
+            maxIntTraj_Q_rotated = np.max([np.max(self.intTraj_rotated[0][1, :]), np.max(self.intTraj_rotated[1][0, :]), np.max(self.intTraj_rotated[2][1, :]), np.max(self.intTraj_rotated[3][1, :])])
+            self.hist2d_gg_rotated, self.binEdges_I_rotated, self.binEdges_Q_rotated = np.histogram2d(self.intTraj_rotated[0][0, :], self.intTraj_rotated[0][1, :], bins=numBins, range=[[minIntTraj_I_rotated, maxIntTraj_I_rotated], [minIntTraj_Q_rotated, maxIntTraj_Q_rotated]])
+
+            intTraj_exc_I_rotated = np.concatenate([self.intTraj_rotated[1][0, :], self.intTraj_rotated[2][0, :], self.intTraj_rotated[3][0, :]])
+            intTraj_exc_Q_rotated = np.concatenate([self.intTraj_rotated[1][1, :], self.intTraj_rotated[2][1, :], self.intTraj_rotated[3][1, :]])
+            self.hist2d_exc_rotated, _ , _ = np.histogram2d(intTraj_exc_I_rotated, intTraj_exc_Q_rotated, bins=numBins, range=[[minIntTraj_I_rotated, maxIntTraj_I_rotated], [minIntTraj_Q_rotated, maxIntTraj_Q_rotated]])
 
 
+            #: plot rotated plots
+            if suppressPlots == False:
+                coordsX_rotated, coordsY_rotated = np.meshgrid(self.binEdges_I_rotated, self.binEdges_Q_rotated)
 
+                axarr[1, 0].pcolormesh(coordsX_rotated, coordsY_rotated, self.hist2d_gg_rotated.T + self.hist2d_exc_rotated.T, cmap='jet')
+                axarr[1, 0].set_title('hist2d of gg and exc, rotated')
 
+                axarr[1, 1].plot(self.binEdges_I_rotated[:-1], self.hist2d_gg_rotated.sum(1))
+                axarr[1, 1].plot(self.binEdges_I_rotated[:-1], self.hist2d_exc_rotated.sum(1))
+                axarr[1, 1].set_title('Integrated I, rotated')
 
+                axarr[1, 2].plot(self.binEdges_Q_rotated[:-1], self.hist2d_gg_rotated.sum(0))
+                axarr[1, 2].plot(self.binEdges_Q_rotated[:-1], self.hist2d_exc_rotated.sum(0))
+                axarr[1, 2].set_title('Integrated Q, rotated')
 
+                # plt.show()
+
+        #: find decBound and trainFid
+        if doRotate:
+            hist2d_gg_chosen = self.hist2d_gg_rotated
+            hist2d_exc_chosen = self.hist2d_exc_rotated
+            binEdges_I_chosen = self.binEdges_I_rotated
+        else:
+            hist2d_gg_chosen = self.hist2d_gg
+            hist2d_exc_chosen = self.hist2d_exc
+            binEdges_I_chosen = self.binEdges_I
+
+        temp_I=[] # temp variable to help calculate the fidelity; it's regarding I quadrature
+        num_gg = hist2d_gg_chosen.sum(1).sum() #number of gg points
+        num_exc = hist2d_exc_chosen.sum(1).sum() #number of exc points
+
+        for k in np.arange(numBins+1):
+            temp_I.append((hist2d_gg_chosen.sum(1)[0:k]/num_gg - hist2d_exc_chosen.sum(1)[0:k]/num_exc).sum())
+        self.decBound_ggexc_I = binEdges_I_chosen[np.argmax(temp_I)] # decision boundary
+        self.trainFid_ggexc_I = (1 + np.max(temp_I)) / 2   #full fid for I quadrature, using naive integration
+
+        print 'trainFid_ggexc_I: ', self.trainFid_ggexc_I
+        print 'decBound_ggexc_I: ', self.decBound_ggexc_I
+
+        if suppressPlots == False:
+            plt.show()
 
 
 
